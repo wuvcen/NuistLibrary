6dae410ca8f2a021f74dddbb6eebcf0224e7616d
王志龙 <991405736qq@gmail.com>
2015-11-20 17:03:05 +0800

    add API URL

---
+++ NuistLibrary/APIURL.h
@@ -0,0 +1,12 @@
+//
+//  APIURL.h
+//  NuistLibrary
+//
+//  Created by 王志龙 on 15/11/20.
+//  Copyright © 2015年 王志龙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define libIndex @"http://lib2.nuist.edu.cn/opac/search.php"
+#define libSearch @"http://lib2.nuist.edu.cn/opac/openlink.php?strSearchType=title&match_flag=forward&historyCount=1&strText={keyword}&doctype=ALL&with_ebook=on&displaypg=10&showmode=list&sort=CATA_DATE&orderby=desc&location=ALL"
