814e251982bbc074c6f6b135654510105fbb7a71
王志龙 <991405736qq@gmail.com>
2015-11-20 18:24:19 +0800

    complete search book api

--- NuistLibrary/SearchBookList.m
+++ NuistLibrary/SearchBookList.m
@@ -7,17 +7,53 @@
 //
 
 #import "SearchBookList.h"
+#import "ListBook.h"
 #import "TFHpple.h"
+
 @implementation SearchBookList
 
 + (void)BookListWithData:(NSData *)data completionBlock:(void (^)(SearchBookList *))block {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 #pragma mark TODO parse in background thread
+        SearchBookList *returnBookList = [SearchBookList new];
+        NSMutableArray *bookList = [NSMutableArray new];
+        TFHpple *doc = [[TFHpple alloc] initWithHTMLData:data];
+        NSArray *elements = [doc searchWithXPathQuery:@"//li[@class='book_list_info']"];
+        @autoreleasepool {
+            for (TFHppleElement *node in elements) {
+                ListBook *book = [ListBook new];
+                
+                TFHppleElement *h3 = [node firstChildWithTagName:@"h3"];
+                book.title = [h3 firstChildWithTagName:@"a"].content;
+                book.url = [[h3 firstChildWithTagName:@"a"] objectForKey:@"href"];
+                
+                TFHppleElement *p = [node firstChildWithTagName:@"p"];
+                book.availble = [p firstChildWithTagName:@"span"].content;
+                
+                [bookList addObject:book];
+            }
+        }
+        returnBookList.bookList = bookList;
+        
+        NSArray *currentgePage = [doc searchWithXPathQuery:@"//*[@id='num']/span/b/font[1]"];
+        if (currentgePage.count >= 1) {
+            TFHppleElement *currentNode = currentgePage.firstObject;
+            returnBookList.currentPage = currentNode.content.integerValue;
+        }
+        
+        NSArray *totalPage = [doc searchWithXPathQuery:@"//*[@id='num']/span/b/font[2]"];
+        if (totalPage.count >= 1) {
+            TFHppleElement *currentNode = totalPage.firstObject;
+            returnBookList.totalPage = currentNode.content.integerValue;
+        }
+        
+        if (block) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                block(returnBookList);
+            });
+        }
+        
     });
 }
 
-- (BOOL)hasNextPage {
-    NSUInteger totalPage = ceil((double)self.total / (double)self.ipp);
-    return totalPage > self.currentPage;
-}
 @end
