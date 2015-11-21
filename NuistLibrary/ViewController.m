9efbee3787bf8155bc27c6b83cc3796fac276f3e
王志龙 <991405736qq@gmail.com>
2015-11-21 16:57:24 +0800

    main vc test

--- NuistLibrary/ViewController.m
+++ NuistLibrary/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "SearchBookList.h"
+#import "BookDetail.h"
 #import "ListBook.h"
 #import "Server.h"
 @interface ViewController ()
@@ -18,13 +19,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [NXServer getSearchBookListByKeyWord:@"爱情" page:@1 completion:^(SearchBookList *list ,NSError *error){
-        if (!error) {
-            for (ListBook *book in list.bookList) {
-                NSLog(@"book %@",book.title);
-            }
-        }
-    }];
+//    [NXServer getSearchBookListByKeyWord:@"爱情" page:@3 completion:^(SearchBookList *list ,NSError *error){
+//        if (!error) {
+//            for (ListBook *book in list.bookList) {
+//                NSLog(@"book %@",book.title);
+//            }
+//            NSString *hasnextpage = [list hasNextPage] ? @"true" : @"false";
+//            NSLog(@"has next page ? %@ %d",hasnextpage, list.totalPage);
+//        }
+//    }];
+//    [NXServer getLibBookDetailByListBook:nil completion:^(BookDetail *bookdetail,NSError *error){
+//        if (!error) {
+//        }
+//    }];
 }
 
 - (void)didReceiveMemoryWarning {
