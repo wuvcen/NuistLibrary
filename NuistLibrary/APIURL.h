18c61386f23a848f2b12cf93ad56c120a8e039c1
王志龙 <991405736qq@gmail.com>
2015-11-21 15:59:28 +0800

    add DoubanInfo API

--- NuistLibrary/APIURL.h
+++ NuistLibrary/APIURL.h
@@ -10,3 +10,6 @@
 
 #define libIndex @"http://lib2.nuist.edu.cn/opac/search.php"
 #define libSearch @"http://lib2.nuist.edu.cn/opac/openlink.php?location=ALL&title={keyword}&doctype=ALL&lang_code=ALL&match_flag=forward&displaypg=10&showmode=list&orderby=DESC&sort=CATA_DATE&onlylendable=no&with_ebook=on&page={page}"
+#define libBookDetailBaseURL @"http://lib2.nuist.edu.cn/opac/"
+/** feature 用于查询 lib 中图书在豆瓣的位置*/
+#define libBookDoubanInfo @"http://lib2.nuist.edu.cn/opac/ajax_douban.php?isbn={isbn}"
