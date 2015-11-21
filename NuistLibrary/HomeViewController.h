35adac6a5bff749d4a29826d964313d53ce5124a
王志龙 <991405736qq@gmail.com>
2015-11-21 18:44:03 +0800

    basic demo

---
+++ NuistLibrary/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  NuistLibrary
+//
+//  Created by 王志龙 on 15/11/21.
+//  Copyright © 2015年 王志龙. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HomeViewController : BaseViewController
+
+@end
---
+++ NuistLibrary/HomeViewController.m
@@ -0,0 +1,53 @@
+//
+//  HomeViewController.m
+//  NuistLibrary
+//
+//  Created by 王志龙 on 15/11/21.
+//  Copyright © 2015年 王志龙. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "Server.h"
+#import "BookDetail.h"
+#import "ListBook.h"
+#import "SearchBookList.h"
+#define reuseIdentifier @"demo"
+
+@interface HomeViewController ()<UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *tableData;
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    [NXServer getSearchBookListByKeyWord:@"ios" page:@1 completion:^(SearchBookList *list, NSError *error){
+        if (!error) {
+            [self.tableData addObjectsFromArray:list.bookList];
+            [self.tableView reloadData];
+        }
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tableData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+    ListBook *book = self.tableData[indexPath.row];
+    cell.textLabel.text = book.title;
+    cell.detailTextLabel.text = [book.availble stringByReplacingOccurrencesOfString:@"\r\n" withString:@""];
+    return cell;
+}
+
+- (NSMutableArray *)tableData {
+    if (!_tableData) {
+        _tableData = [NSMutableArray new];
+    }
+    return _tableData;
+}
+@end
